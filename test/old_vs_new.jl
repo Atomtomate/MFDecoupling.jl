ac68ccfeaf1bd6c9fd539f2c81a3618d1eabeea6
Julian Stobbe <a@b.c>
2023-12-21 16:47:27 +0000

    added more tests

--- test/old_vs_new.jl
+++ test/old_vs_new.jl
@@ -3,35 +3,36 @@ include("rhs_old.jl")
     NSamples = 2
     ptest = collect(LinRange(1,3,5))
     t = 0.1
-    LL = 10
-    ptest[1] = 10
+    for LL in [10,20,30]
+        ptest[1] = LL
 
 
-    Q,P,LC,LK,LIm = MFDecoupling.gen_helpers(LL)
-    for i in 1:NSamples
-        X0test = randn(ComplexF64, 120)
-        X0test[1] = real(X0test[1])
-        X0test[5] = real(X0test[5])
-        X0test[8] = real(X0test[8])
-        X0test[10] = real(X0test[10])
+        Q,P,LC,LK,LIm = MFDecoupling.gen_helpers(LL)
+        for i in 1:NSamples
+            X0test = randn(ComplexF64, (LC+LK+10))
+            X0test[1] = real(X0test[1])
+            X0test[5] = real(X0test[5])
+            X0test[8] = real(X0test[8])
+            X0test[10] = real(X0test[10])
 
-        for i in 1:LL
-            Qii=floor(Int,(10+1)*i - (i+1)*i/2 + i +10)
-            X0test[Qii] = real(X0test[Qii])
+            for i in 1:LL
+                Qii=floor(Int,(LL+1)*i - (i+1)*i/2 + i + 10)
+                X0test[Qii] = real(X0test[Qii])
+            end
+            @test !any(isnan.(X0test))
+
+            dX0test_1 = similar(X0test)
+            dX0test_2 = similar(X0test)
+            fill!(dX0test_1, NaN)
+            fill!(dX0test_2, NaN)
+
+            MFDecoupling.rhs!(dX0test_1, X0test, ptest, t, LC, LK, LIm, Q, P)
+            test!(dX0test_2, X0test, ptest, t)
+
+            @test !any(isnan.(dX0test_1))
+            @test !any(isnan.(dX0test_2))
+            @test all(dX0test_1 .≈ dX0test_2)
         end
-        @test !any(isnan.(X0test))
-
-        dX0test_1 = similar(X0test)
-        dX0test_2 = similar(X0test)
-        fill!(dX0test_1, NaN)
-        fill!(dX0test_2, NaN)
-
-        MFDecoupling.rhs!(dX0test_1, X0test, ptest, t, LC, LK, LIm, Q, P)
-        test!(dX0test_2, X0test, ptest, t)
-
-        @test !any(isnan.(dX0test_1))
-        @test !any(isnan.(dX0test_2))
-        @test all(dX0test_1 .≈ dX0test_2)
     end
 end
 
@@ -43,43 +44,27 @@ end
     NSamples = 2
     ptest = collect(LinRange(1,3,5))
     t = 0.1
-    LL = 10
-    ptest[1] = 10
-    Q,P,LC,LK,LIm = MFDecoupling.gen_helpers(LL)
+    for LL in [10,20,30]
+        ptest[1] = LL
+        Q,P,LC,LK,LIm = MFDecoupling.gen_helpers(LL)
 
 
-    for i in 1:NSamples
-        X0test = randn(Float64, 240)
+        for i in 1:NSamples
+            X0test = randn(Float64, 2*(LC+LK+10))
 
 
-        dX0test_1 = similar(X0test)
-        dX0test_2 = similar(X0test)
-        fill!(dX0test_1, NaN)
-        fill!(dX0test_2, NaN)
+            dX0test_1 = similar(X0test)
+            dX0test_2 = similar(X0test)
+            fill!(dX0test_1, NaN)
+            fill!(dX0test_2, NaN)
 
-        rhs_real_test1!(dX0test_1, X0test, ptest, t)
-        MFDecoupling.rhs_real!(dX0test_2, X0test, ptest, t, LC, LK, LIm, Q, P)
+            rhs_real_test1!(dX0test_1, X0test, ptest, t)
+            MFDecoupling.rhs_real!(dX0test_2, X0test, ptest, t, LC, LK, LIm, Q, P)
 
 
-        """
-        println("rho")
-        for index in 1:10
-            println(index,"   ",dX0test_1[index] - dX0test_2[index],"  ",dX0test_1[index+120] - dX0test_2[index+120])
+           @test !any(isnan.(dX0test_1))
+           @test !any(isnan.(dX0test_2))
+           @test all(dX0test_1 .≈ dX0test_2)
         end
-        println("C")
-        for index in 11:75
-            println(index,"   ",dX0test_1[index] - dX0test_2[index],"  ",dX0test_1[index+120] - dX0test_2[index+120])
-        end
-
-        println("K")
-        for index in 76:120
-            println(index,"   ",dX0test_1[index] - dX0test_2[index],"  ",dX0test_1[index+120] - dX0test_2[index+120])
-        end
-        """
-
-
-       @test !any(isnan.(dX0test_1))
-       @test !any(isnan.(dX0test_2))
-       @test all(dX0test_1 .≈ dX0test_2)
     end
 end
