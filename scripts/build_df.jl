5dd92e4360de70b386ae37be72d0a0365449039f
Julian Stobbe <a@b.c>
2023-12-29 14:33:32 +0000

    modified build_df script

--- scripts/build_df.jl
+++ scripts/build_df.jl
@@ -1,3 +1,10 @@
+# Builds DataFrame from file obtained by merge.jl (after Uscan). 
+# Expects .jld2 file (output of merge.jl) with data and path to file where dataframe will be stored.
+# Example usage: julia build_df out_full.jld2 out_df.jld2
+
+fn = ARGS[1]
+dfFile = ARGS[2]
+
 function build_df(fn::String)
     df = DataFrame(U = Float64[], V = Float64[], t = Vector{Float64}[],        rho11 = Vector{ComplexF64}[], rho12 = Vector{ComplexF64}[],
                    rho13 = Vector{ComplexF64}[], rho14 = Vector{ComplexF64}[], rho22 = Vector{ComplexF64}[], rho23 = Vector{ComplexF64}[],
@@ -29,33 +36,40 @@ function build_df(fn::String)
     else
         error("$fn is not a result file or directory!")
     end
-
     df[!,:f_up] = df[:,:rho13] .+ df[:,:rho24]
     df[!,:f_do] = df[:,:rho12] .+ df[:,:rho34]
-
+    
     # supplement mean value
-    lim_ind = 1000
+    lim_ind = 10000
     var_window = trunc(Int, length(df[1,:t]) / 10)
     for cn in names(df)[4:end]
         df[!,Symbol(cn * "_lim")] = map(x-> mean(x[end-lim_ind:end]), df[:,Symbol(cn)])
         df[!,Symbol(cn * "_std")] = map(x-> [std(x[i-var_window:i]) for i in var_window+1:length(x)], df[:,Symbol(cn)])
     end
-
+    
     # calculate ffts of time series
-    fft_cols = ["rho11", "f_up", "C01"]
+    fft_cols = ["rho11", "rho12", "rho22", "rho24", "f_up", "C01"]
+    fft_start_i = length(df[1,:t])-trunc(Int, length(df[1,:t]) / 10)
+    tx_fft = df[1,:t][fft_start_i:end]
+    freqs = collect(FourierAnalysis.fftfreq(length(tx_fft), 1.0/diff(tx_fft)[1]) |> FourierAnalysis.fftshift)
+    df[!,:freqs] = [freqs for i in 1:size(df,1)]
     for cl in fft_cols
-        df[!,Symbol(cl * "_fft")] = map(x-> FourierAnalysis.fft(real(x[1] .- x[2])) ./ length(x[1]) |> FourierAnalysis.fftshift, eachrow(df[:,[Symbol(cl),Symbol(cl * "_lim")]]))
+        df[!,Symbol(cl * "_fft")] = map(x-> FourierAnalysis.fft(x[1][fft_start_i:end] .- x[2]) ./ length(x[1]) |> FourierAnalysis.fftshift, eachrow(df[:,[Symbol(cl),Symbol(cl * "_lim")]]))
+        df[!,Symbol(cl * "_fft_zm")] = map(x-> FourierAnalysis.fft(x[fft_start_i:end]) ./ length(x[fft_start_i:end]) |> FourierAnalysis.fftshift, df[:,Symbol(cl)])
     end
 
     return df
 end
-df_pt= build_df(fn_pt);
-df_coarse = build_df(fn_coarse);
-df_smallU = build_df(fn_smallU);
 
-# combine and remove duplicates
-df = combine(groupby(vcat(df_pt,df_coarse, df_smallU), [:U, :V]), last); 
+df = if isfile(dfFile)
+    jldopen(dfFile) do ff
+        ff["df"]
+    end
+else
+    df_precision = build_df(fn);
+end
 
+df_Vin = df[df.V .≈ Vin, :]
 sort!(df_Vin, [:U])
 
 VList = sort(unique(df.V))
